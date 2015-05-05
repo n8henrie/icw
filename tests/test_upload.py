54aaa1f1dcc27de3cda6103ea3a1b30a2b574048
Nathan Henrie <n8henrie@gmail.com>
2015-05-05 14:17:37 -0600

    Removed deprecated test_upload.

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -3,32 +3,9 @@ py.test tests to test a number of previously problematic files
 """
 
 from StringIO import StringIO
-import os.path
 
 proper_headers = ['End Date', 'Description', 'All Day Event', 'Start Time',
                   'Private', 'End Time', 'Location', 'Start Date', 'Subject']
-
-error_dir = os.path.join(os.path.dirname(__file__), 'error_files')
-success_dir = os.path.join(os.path.dirname(__file__), 'success_files')
-
-test_files = [
-    '/Users/n8henrie/Dropbox/Launch/HomeMade/.virtualenvs/'
-    'icsConverterWebapp/postmortems/20130806_Rock_View_Elementary.csv',
-    ]
-
-
-def test_post_from_file_path(client):
-    """Iterate through the list of test_files and make sure they convert
-    without errors.
-    """
-
-    for test_file in test_files:
-        with open(test_file) as r:
-            response = client.post('/', data=dict(csv_file=r),
-                                   follow_redirects=True)
-
-        download = client.get('/download')
-        assert 'END:VEVENT\r\nEND:VCALENDAR' in download.data
 
 
 def test_from_string(client):
