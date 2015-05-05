3e6f59075632a9f2ef80d6bca7d2c492e536a2c2
Nathan Henrie <n8henrie@gmail.com>
2015-05-05 13:46:11 -0600

    First working version. GAE depends on main.py.

--- main.py
+++ main.py
@@ -1,25 +1,4 @@
-"""`main` is the top level module for your Flask application."""
+from google.appengine.ext.webapp.util import run_wsgi_app
+from icw import app
 
-# Import the Flask Framework
-from flask import Flask
-app = Flask(__name__)
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
-
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, Nothing at this URL.', 404
-
-
-@app.errorhandler(500)
-def page_not_found(e):
-    """Return a custom 500 error."""
-    return 'Sorry, unexpected error: {}'.format(e), 500
+run_wsgi_app(app)
