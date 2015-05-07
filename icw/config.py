754da8c406127b3aa465ee7d73e6e618849860a2
Nathan Henrie <n8henrie@gmail.com>
2015-05-07 16:41:09 -0600

    Added default config object.

---
+++ icw/config.py
@@ -0,0 +1,9 @@
+"""config.py
+Contains the default config class for icw.
+"""
+
+
+class DefaultConfig(object):
+    DEBUG = False
+    ALLOWED_EXTENSIONS = set(['csv'])
+    SECRET_KEY = 'SUPER_SECRET_KEY'
