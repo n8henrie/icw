292bf62b6f2beee0b1464b9f1f1ced6b36572f02
Nathan Henrie <nate@n8henrie.com>
2022-09-09 08:47:58 -0600

    No longer using travis

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,7 +5,3 @@ from icw import app
 
 def test_config_read():
     assert app.config["ALLOWED_EXTENSIONS"] == set(["csv"])
-    if os.getenv("TRAVIS") == "true":
-        assert app.config["SECRET_KEY"] == "SUPER_SECRET_KEY"
-    else:
-        assert app.config["SECRET_KEY"] != "SUPER_SECRET_KEY"
